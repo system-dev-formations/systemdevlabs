af25dbfaf2b707a90df83a64ad6ccc321eea3c33
Herve <dockerlite@gmail.com>
2020-12-23 07:14:32 +0100

    Added foreground.sh and background.sh files

--- ansible-beginner/background.sh
+++ ansible-beginner/background.sh
@@ -1,6 +1,1 @@
-apt update
-apt upgrade
-apt install software-properties-common
-apt-add-repositories ppa:ansible/ansible
-apt update
-apt install ansible
+git clone http://github.com/master-afip/ansible-examples.git
