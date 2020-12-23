d2719c5961ad06f0755b0f43b011381e32d60fb6
Herve <dockerlite@gmail.com>
2020-12-23 16:36:42 +0100

    Refactored and added a progress spinner

--- ansible-beginner/background.sh
+++ ansible-beginner/background.sh
@@ -3,3 +3,5 @@ apt -y install ansible
 docker run -d --name target1 systemdevformations/ubuntu_ssh:v2
 docker run -d --name target2 systemdevformations/centos_ssh:v5
 docker run -d --name target3 --env ROOT_PASSWORD=Passw0rd systemdevformations/alpine-ssh:v1
+echo done >> /root/katacoda-finished
+echo done >> /root/katacoda-background-finished
--- ansible-beginner/foreground.sh
+++ ansible-beginner/foreground.sh
@@ -1,1 +1,2 @@
 git clone http://github.com/master-afip/ansible-examples.git
+sleep 1; wait.sh
