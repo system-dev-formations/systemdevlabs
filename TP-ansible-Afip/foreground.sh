45dc675084f3a9271bf21ce019ba86059e307a32
Herve <dockerlite@gmail.com>
2021-02-09 08:40:41 +0100

    Added git completion

--- TP-ansible-Afip/foreground.sh
+++ TP-ansible-Afip/foreground.sh
@@ -1,6 +1,3 @@
 git clone http://github.com/master-afip/ansible-examples.git
 alias ll='ls -alrt'
 sleep 1; wait.sh
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-source ~/.git-completion.bash
-PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$
