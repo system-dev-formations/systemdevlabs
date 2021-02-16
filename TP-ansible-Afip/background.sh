1d7e50d7165126af4030f6d65bf48c6f8ce845c7
Herve <dockerlite@gmail.com>
2021-02-16 06:59:16 +0100

    Removed background

--- TP-ansible-Afip/background.sh
+++
@@ -1,9 +0,0 @@
-apt update
-apt -y install ansible python
-pip3 install natsort
-docker run -d --name target1 systemdevformations/ubuntu_ssh:v2
-docker run -d --name target2 systemdevformations/centos_ssh:v5
-docker run -d --name target3 --env ROOT_PASSWORD=Passw0rd systemdevformations/alpine-ssh:v1
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-echo done >> /root/katacoda-finished
-echo done >> /root/katacoda-background-finished
--- TP-ansible-Afip/foreground.sh
+++ TP-ansible-Afip/foreground.sh
@@ -1,5 +1,11 @@
+apt update
+apt -y install ansible python
+pip3 install natsort
+docker run -d --name target1 systemdevformations/ubuntu_ssh:v2
+docker run -d --name target2 systemdevformations/centos_ssh:v5
+docker run -d --name target3 --env ROOT_PASSWORD=Passw0rd systemdevformations/alpine-ssh:v1
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 git clone http://github.com/master-afip/ansible-examples.git
-sleep 1; wait.sh
 source ~/.git-completion.bash
 alias ll='ls -alrt'
 PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
