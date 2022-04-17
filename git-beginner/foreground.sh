50b456cb368d1f78307288e7b037f45e549c9a9c
Herve <dockerlite@gmail.com>
2022-04-17 18:11:24 +0200

    Added a go web server

--- git-beginner/foreground.sh
+++ git-beginner/foreground.sh
@@ -1,3 +1,8 @@
 apt update
 git clone https://github.com/git/git.git
+git clone https://github.com/crunchy-devops/go-web-server.git
+cd go-web-server && docker build -t go-web-server .
+cd go-web-server && docker run go-web-server > go-web-server
+chmod +x go-web-server
 apt -y install tig
+cd ~
