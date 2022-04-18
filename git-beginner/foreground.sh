c6729b99552e6937011235a49f5733189e12aac0
Herve <dockerlite@gmail.com>
2022-04-18 09:31:24 +0200

    changed foreground with chained linux commands

--- git-beginner/foreground.sh
+++ git-beginner/foreground.sh
@@ -4,6 +4,4 @@ git clone https://github.com/crunchy-devops/go-web-server.git
 cd go-web-server && docker build -t go-web-server .
 cd go-web-server && docker run go-web-server > go-web-server
 chmod +x go-web-server
-apt -y install tig
-clear
-cd ~
+apt -y install tig && clear   && cd ~
