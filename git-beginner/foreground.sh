3ae3d45a86603837095cd70ce18fcec2c5b771bc
Herve <dockerlite@gmail.com>
2022-04-30 16:38:32 +0200

    Add VSCode IDE

--- git-beginner/foreground.sh
+++ git-beginner/foreground.sh
@@ -7,3 +7,12 @@ alias ll='ls -alrt'
 PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
 pip3 install flask
 apt -y install tig && clear && cd ~
+EXTENSION_DIR=/opt/.katacodacode/extensions/humao.rest-client-0.24.3
+while ! [ -d "$EXTENSION_DIR" ];
+do
+  clear;
+  echo 'Installing VS Code extensions, please wait before opening the IDE'.
+  sleep 1
+done
+clear
+: VS Code extensions installed,
