11dcf5e14c41b6c43fea4808eec94877682aab10
Herve <dockerlite@gmail.com>
2022-05-13 11:04:16 +0200

    Use alpine v2

--- ansible-airbus/foreground.sh
+++ ansible-airbus/foreground.sh
@@ -7,9 +7,10 @@ pip3 install wheel
 pip3 install docker-compose
 docker run -d --name target1 systemdevformations/ubuntu_ssh:v2
 docker run -d --name target2 systemdevformations/centos_ssh:v5
-docker run -d --name target3 --env ROOT_PASSWORD=Passw0rd systemdevformations/alpine-ssh:v1
+docker run -d --name target3 --env ROOT_PASSWORD=Passw0rd systemdevformations/alpine-ssh:v2
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 git clone http://github.com/crunchy-devops/ansible-course.git
 source ~/.git-completion.bash
 alias ll='ls -alrt'
 PS1='[\u@\h \W$(__git_ps1 " (%s)")]\$ '
+clear
